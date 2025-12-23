8ae69e3ef4e14808e738d34fd568d705e58d1b6c
congnh <nguyenhuucongzz01@gmail.com>
2025-12-23 17:00:51 +0700

    final

--- main.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-
-train_behaviors = pl.read_parquet("processed_parquet/train/behaviors_processed.parquet")
-train_history = pl.read_parquet("processed_parquet/train/history_processed.parquet")
-
-for idx, x in enumerate(train_behaviors.sample(n=1)):
-    print(x)
-
